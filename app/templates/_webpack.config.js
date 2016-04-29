857b969933a8ef2639d67a52f175173fe6381c98
Peach <scdzwyxst@gmail.com>
2016-04-29 10:16:48 +0800

    Add json/html/font load support

--- app/templates/_webpack.config.js
+++ app/templates/_webpack.config.js
@@ -22,7 +22,22 @@ module.exports = {
         loader: 'babel'
       },
       {
+        test: /\.json$/,
+        loader: 'json'
+      },
+      {
+        test: /\.html$/,
+        loader: 'vue-html'
+      },
+      {
         test: /\.(png|jpe?g|gif|svg)(\?.*)?$/,
+        loader: 'url',
+        query: {
+          limit: 10000
+        }
+      },
+      {
+        test: /\.(woff2?|eot|ttf|otf)(\?.*)?$/,
         loader: 'url',
         query: {
           limit: 10000
