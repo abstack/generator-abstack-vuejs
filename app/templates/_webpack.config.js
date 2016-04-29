c4d415cff1617707c7c1ee98f608241a5b50ec97
Peach <scdzwyxst@gmail.com>
2016-04-29 13:52:13 +0800

    Add the output path for large image files and font files

--- app/templates/_webpack.config.js
+++ app/templates/_webpack.config.js
@@ -44,14 +44,16 @@ module.exports = {
         test: /\.(png|jpe?g|gif|svg)(\?.*)?$/,
         loader: 'url',
         query: {
-          limit: 10000
+          limit: 10000,
+          name: 'static/img/[name]_[hash:7].[ext]'
         }
       },
       {
         test: /\.(woff2?|eot|ttf|otf)(\?.*)?$/,
         loader: 'url',
         query: {
-          limit: 10000
+          limit: 10000,
+          name: 'static/fonts/[name]_[hash:7].[ext]'
         }
       }
     ]
